fb73542aaa1aaa20fc55cb91160027ae96095820
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-01-19 21:45:03 +0900

    Update: rails g 生成ファイルをslimに指定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module PdfToCsv
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators.template_engine = :slim
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
